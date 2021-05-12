04e0420acab69fa52335b5b7d29289b7cee6ec4d
unknown <thieuquangnghia2000@gmail.com>
2021-05-12 16:56:11 +0700

    first commit

---
+++ app/src/main/java/com/example/a18020331_nguyenxuanhai_roomdb/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a18020331_nguyenxuanhai_roomdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "User").allowMainThreadQueries().build();
+
+        UserDao userDao = db.userDao();
+        List<User> users = userDao.getAll();
+    }
+}
---
+++ app/src/main/java/com/example/a18020331_nguyenxuanhai_roomdb/UserDao.java
@@ -0,0 +1,27 @@
+package com.example.a18020331_nguyenxuanhai_roomdb;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM users")
+    List<User> getAll();
+
+    @Query("SELECT * FROM users WHERE uid IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM users WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
